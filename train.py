124196395162bad3adbe94376262eaacf4e36ffb
Ren <ryanzjlib@gmail.com>
2021-02-28 15:46:50 -0500

    explicitly specify num_subspaces

--- train.py
+++ train.py
@@ -13,6 +13,7 @@ from sklearn.metrics import mean_squared_error
 # Hyper-params
 model_params = dict(
     hidden_size=1024,
+    num_subspaces=8,
     embed_dim=128,
     num_heads=8,
     dropout=0,
@@ -33,18 +34,18 @@ swap_probas = sum([[p] * r for p, r in zip(probas, repeats)], [])
 X, Y, n_cats, n_nums = get_data()
 
 train_dl = DataLoader(
-    dataset=SingleDataset(X), 
-    batch_size=batch_size, 
-    shuffle=True, 
-    pin_memory=True, 
+    dataset=SingleDataset(X),
+    batch_size=batch_size,
+    shuffle=True,
+    pin_memory=True,
     drop_last=True
 )
 
 # setup model
 model = TransformerAutoEncoder(
-    num_inputs=X.shape[1], 
-    n_cats=n_cats, 
-    n_nums=n_nums, 
+    num_inputs=X.shape[1],
+    n_cats=n_cats,
+    n_nums=n_nums,
     **model_params
 ).cuda()
 model_checkpoint = 'model_checkpoint.pth'
@@ -69,7 +70,7 @@ for epoch in range(max_epochs):
         optimizer.step()
 
         meter.update(loss.detach().cpu().numpy())
-    
+
     delta = (datetime.now() - t0).seconds
     scheduler.step()
     print('\r epoch {:5d} - loss {:.6f} - {:4.6f} sec per epoch'.format(epoch, meter.avg, delta), end='')
@@ -87,8 +88,8 @@ model.load_state_dict(model_state['model'])
 dl = DataLoader(dataset=SingleDataset(X), batch_size=1024, shuffle=False, pin_memory=True, drop_last=False)
 features = []
 model.eval()
-with torch.no_grad(): 
-    for x in dl: 
+with torch.no_grad():
+    for x in dl:
         features.append(model.feature(x.cuda()).detach().cpu().numpy())
 features = np.vstack(features)
 
